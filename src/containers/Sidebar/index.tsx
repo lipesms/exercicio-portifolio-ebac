1f95eb44d9068b5afb4b62fa95f3def404158490
Felipe da Silva Martins <femartins2010@gmail.com>
2023-11-07 14:44:18 -0300

    criando layout da página

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/lipesms.png" alt="foto de perfil" />
+    <Titulo>Felipe Martins</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre</Titulo>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure,
+      exercitationem recusandae! Itaque, laborum culpa rerum quos quis
+      cupiditate repudiandae et sit, sint, harum perspiciatis! Voluptate minus
+      eius totam error delectus?
+    </p>
+  </section>
+)
+
+export default Sobre
